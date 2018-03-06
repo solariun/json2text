4d51d261abecde6cc793ca6377c238dcb149e49c
Apple <gustavocampos@gustavos-mac-mini.local>
2018-03-06 08:15:00 -0300

    Initial Commit

---
+++ json2text/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  json2text
+//
+//  Created by GUSTAVO CAMPOS on 06/03/18.
+//  Copyright © 2018 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
